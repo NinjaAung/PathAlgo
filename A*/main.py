a923ab197bcacb18b204b8ae100b07750ccc31ac
Nyein Chan Aung <ninja.aung02@gmail.com>
2021-07-18 16:01:02 -0700

    Init Astar method

---
+++ A*/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# A* pathfinding algo
+'''
+A* Score = Cost Of Path + Heurestic
+
+Unlike Djik we are using an additional heurestic with equals the A* score 
+This heurestic is adding E(Sum of all edges of current path) and the A(The heuristic distance) to the goal
+
+Important about the A(Heuristic) is that it MUST never over estimate the cost
+'''
+
+# Code
+
+def astar_static(table,start):
+    pass
+
+
+def astar_grid(table,star,end):
+    pass
+
